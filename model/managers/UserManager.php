804b2df1f091ea6f8f733b5df640e1541f7255a2
Kevin ZITNIK <zitnik.kevin@gmail.com>
2023-06-28 23:47:29 +0200

    feat: add sql select for user in topics and posts

--- model/managers/UserManager.php
+++ model/managers/UserManager.php
@@ -15,5 +15,29 @@
             parent::connect();
         }
 
+        public function findUserByTopicId($id){
 
+            $sql = "SELECT *
+                    FROM ".$this->tableName." u
+                    INNER JOIN topic t ON t.user_id = u.id_user
+                    WHERE t.id_topic = :id";
+
+            return $this->getOneOrNullResult(
+                DAO::select($sql, ['id' => $id], false), 
+                $this->className
+            );
+        }
+
+        public function findUserByPostId($id){
+
+            $sql = "SELECT *
+                    FROM ".$this->tableName." u
+                    INNER JOIN post p ON p.user_id = u.id_user
+                    WHERE p.id_post = :id";
+
+            return $this->getOneOrNullResult(
+                DAO::select($sql, ['id' => $id], false), 
+                $this->className
+            );
+        }
     }
