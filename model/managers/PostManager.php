98052dfe527e81eb75de409db4923d450677f12a
Kevin ZITNIK <zitnik.kevin@gmail.com>
2023-06-28 23:48:38 +0200

    feat: add sql select for post in topics

--- model/managers/PostManager.php
+++ model/managers/PostManager.php
@@ -15,5 +15,16 @@
             parent::connect();
         }
 
+        public function findPostsByTopicId($id){
 
+            $sql = "SELECT *
+                    FROM ".$this->tableName." p
+                    INNER JOIN topic t ON t.id_topic = p.topic_id
+                    WHERE t.id_topic = :id";
+
+            return $this->getMultipleResults(
+                DAO::select($sql, ['id' => $id], true), 
+                $this->className
+            );
+        }
     }
