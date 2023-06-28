6dc4299fc1ec742a186038887219e24e64496fae
Kevin ZITNIK <zitnik.kevin@gmail.com>
2023-06-28 23:49:21 +0200

    feat: add rows by PDO results for detailTopic

--- view/forum/detailTopic.php
+++ view/forum/detailTopic.php
@@ -1,9 +1,25 @@
 <?php
 
+$user = $result["data"]['user'];
+$userPosts = $result["data"]['userPosts'];
 $topics = $result["data"]['topics'];
-
+$posts = $result["data"]['posts'];
 
 ?>
 
-<?=$topics->getTitle()?> <br/>
-<?=$topics->getTextTopic()?>
+<section class="topic-author">
+<p><?=$user->getUsername()?></p>
+<h1><?=$topics->getTitle()?></h1> 
+<p><?=$topics->getTextTopic()?></p>
+</section>
+
+<?php
+foreach($posts as $post){
+
+?>
+<div class="topic-posts">
+<p><?=$userPosts->getUsername()?></p>
+<p><?=$post->getText()?></p>
+</div>
+<?php
+}
