c2d255b09033d07d74f5b3450546cb1336f1eac1
Kevin ZITNIK <zitnik.kevin@gmail.com>
2023-06-28 19:01:27 +0200

    feat: add lists and details for topic and category

--- controller/ForumController.php
+++ controller/ForumController.php
@@ -5,6 +5,7 @@
     use App\Session;
     use App\AbstractController;
     use App\ControllerInterface;
+    use Model\Managers\CategoryManager;
     use Model\Managers\TopicManager;
     use Model\Managers\PostManager;
     
@@ -12,18 +13,53 @@
 
         public function index(){
           
-
-           $topicManager = new TopicManager();
-
-            return [
-                "view" => VIEW_DIR."forum/listTopics.php",
-                "data" => [
-                    "topics" => $topicManager->findAll(["creationdate", "DESC"])
-                ]
-            ];
-        
+            $categoryManager = new CategoryManager();
+            
+                return [
+                    "view" => VIEW_DIR."forum/listCategories.php",
+                    "data" => [
+                        "categories" => $categoryManager->findAll(["categoryName", "ASC"])
+                    ]
+                ];
+            
         }
 
-        
+        public function listTopics() {
+            
+            $topicManager = new TopicManager();
+            
+                return [
+                    "view" => VIEW_DIR."forum/listTopics.php",
+                    "data" => [
+                        "topics" => $topicManager->findAll(["creationdate", "DESC"])
+                    ]
+                ];
+            
+        }
 
-    }
+        public function detailCategory($id) {
+            
+            $topicManager = new TopicManager();
+            
+                return [
+                    "view" => VIEW_DIR."forum/detailCategory.php",
+                    "data" => [
+                        "topics" => $topicManager->findTopicsByCategoryId($id)
+                    ]
+                ];
+            
+        }
+        
+        public function detailTopic($id) {
+            
+            $topicManager = new TopicManager();
+        
+                return [
+                    "view" => VIEW_DIR."forum/detailTopic.php",
+                    "data" => [
+                        "topics" => $topicManager->findOneById($id)
+                    ]
+                ];
+            
+        }
+}
