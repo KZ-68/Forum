f8f97cf93378c7660ddf0bacff85b649c7f8b43c
Kevin ZITNIK <zitnik.kevin@gmail.com>
2023-06-29 23:49:28 +0200

    fix: fixed sql request

--- model/managers/TopicManager.php
+++ model/managers/TopicManager.php
@@ -15,22 +15,13 @@
             parent::connect();
         }
 
-        public function findTopicsId($id){
-
-            $sql = "SELECT *
-                    FROM ".$this->tableName." t
-                    INNER JOIN category c ON c.id_category = t.category_id
-                    WHERE t.id_topic = :id";
-
-            return $this->getMultipleResults(
-                DAO::select($sql, ['id' => $id], true), 
-                $this->className
-            );
-        }
-
         public function findTopicsByCategoryId($id){
 
-            $sql = "SELECT *
+            $sql = "SELECT 
+                        t.id_topic,
+                        t.title,
+                        t.category_id,
+                        t.creationdate
                     FROM ".$this->tableName." t
                     INNER JOIN category c ON c.id_category = t.category_id
                     WHERE c.id_category = :id";
@@ -43,7 +34,12 @@
 
         public function findTopicsByUserId($id){
 
-            $sql = "SELECT *
+            $sql = "SELECT
+                        t.id_topic,
+                        t.title,
+                        t.user_id,
+                        t.creationdate,
+                        u.username
                     FROM ".$this->tableName." t
                     INNER JOIN user u ON u.id_user = t.user_id
                     WHERE u.id_user = :id";
