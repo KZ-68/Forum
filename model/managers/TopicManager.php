424dce41f9c8351dfc07f1e3fe2c596a4883161a
Kevin ZITNIK <zitnik.kevin@gmail.com>
2023-06-29 13:00:56 +0200

    feat: add methods for detailUser

--- model/managers/TopicManager.php
+++ model/managers/TopicManager.php
@@ -15,12 +15,38 @@
             parent::connect();
         }
 
+        public function findTopicsId($id){
+
+            $sql = "SELECT *
+                    FROM ".$this->tableName." t
+                    INNER JOIN category c ON c.id_category = t.category_id
+                    WHERE t.id_topic = :id";
+
+            return $this->getMultipleResults(
+                DAO::select($sql, ['id' => $id], true), 
+                $this->className
+            );
+        }
+
         public function findTopicsByCategoryId($id){
 
             $sql = "SELECT *
                     FROM ".$this->tableName." t
                     INNER JOIN category c ON c.id_category = t.category_id
                     WHERE c.id_category = :id";
+
+            return $this->getMultipleResults(
+                DAO::select($sql, ['id' => $id], true), 
+                $this->className
+            );
+        }
+
+        public function findTopicsByUserId($id){
+
+            $sql = "SELECT *
+                    FROM ".$this->tableName." t
+                    INNER JOIN user u ON u.id_user = t.user_id
+                    WHERE u.id_user = :id";
 
             return $this->getMultipleResults(
                 DAO::select($sql, ['id' => $id], true), 
