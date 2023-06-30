c87b4e5281b5b6bca2eb450301826954295762c8
Kevin ZITNIK <zitnik.kevin@gmail.com>
2023-06-30 17:13:36 +0200

    feat: add foreach to display posts by user + fix access by id for posts

--- view/forum/detailUser.php
+++ view/forum/detailUser.php
@@ -2,6 +2,7 @@
 
 $users = $result["data"]['users'];
 $topicsUser = $result["data"]['topicsUser'];
+$postsUser = $result["data"]['postsUser'];
 
 ?>
 
@@ -10,9 +11,23 @@ $topicsUser = $result["data"]['topicsUser'];
 <p>Registration Date : <?=$users->getRegistrationdate()?></p>
 
 <p>Topics created by this user :</p>
+
 <?php
 foreach($topicsUser as $topicUser) {
 ?>
-<p><a href="index.php?ctrl=forum&action=detailTopic&id=<?=$topicUser->getId()?>"><?=$topicUser->getTitle()?></a></p>
+    <p><a href="index.php?ctrl=forum&action=detailTopic&id=<?=$topicUser->getId()?>"><?=$topicUser->getTitle()?></a></p>
 <?php
 }
+?>
+
+<p>Posts created by this user :</p>
+
+<?php
+foreach($postsUser as $postUser) {
+?>
+    <p><a href="index.php?ctrl=forum&action=detailTopic&id=<?=$postUser->getTopic()->getId()?>"><?=$postUser->getText()?></a></p>
+<?php
+}
+?>
+
+<button class="home-btn"><a href="index.php?ctrl=home&action=home.php">Return Home</a></button>
