db737a0a72337e19c31b80254930bc3e1d39a95b
Kevin ZITNIK <zitnik.kevin@gmail.com>
2023-06-30 17:15:36 +0200

    feat: add anchor link for home and createTopic

--- view/forum/detailCategory.php
+++ view/forum/detailCategory.php
@@ -8,8 +8,12 @@ $topicsCategory = $result["data"]['topicsCategory'];
 
 foreach($topicsCategory as $topicCategory){
 ?>
-<p><a href="index.php?ctrl=forum&action=detailTopic&id=<?=$topicCategory->getId()?>"><?=$topicCategory->getTitle()?></a></p>
-<p>Creation Date : <?=$topicCategory->getCreationdate()?></p>
+    <p><a href="index.php?ctrl=forum&action=detailTopic&id=<?=$topicCategory->getId()?>"><?=$topicCategory->getTitle()?></a></p>
+    <p>Creation Date : <?=$topicCategory->getCreationdate()?></p>
 <?php
 }
+?>
 
+<button class="createTopic-btn"><a href="index.php?ctrl=forum&action=createTopic">Create Topic</a></button>
+
+<button class="home-btn"><a href="index.php?ctrl=home&action=home.php">Return Home</a></button>
