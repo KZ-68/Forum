994bac0857928e616f0b7812e43f99f6b38ab8b5
Kevin ZITNIK <zitnik.kevin@gmail.com>
2023-06-30 17:17:20 +0200

    fix select sql requests

--- model/managers/PostManager.php
+++ model/managers/PostManager.php
@@ -17,10 +17,39 @@
 
         public function findPostsByTopicId($id){
 
-            $sql = "SELECT *
+            $sql = "SELECT 
+                        p.id_post,
+                        p.topic_id,
+                        p.user_id,
+                        p.text,
+                        p.creationdate,
+                        t.id_topic,
+                        u.id_user,
+                        u.username
                     FROM ".$this->tableName." p
                     INNER JOIN topic t ON t.id_topic = p.topic_id
+                    INNER JOIN user u ON u.id_user = p.user_id
                     WHERE t.id_topic = :id";
+
+            return $this->getMultipleResults(
+                DAO::select($sql, ['id' => $id], true), 
+                $this->className
+            );
+        }
+
+        public function findPostsByUserId($id){
+
+            $sql = "SELECT
+                        p.id_post,
+                        p.user_id,
+                        p.topic_id,
+                        p.text,
+                        p.creationdate,
+                        u.username
+                    FROM ".$this->tableName." p
+                    INNER JOIN user u ON u.id_user = p.user_id
+                    INNER JOIN topic t ON t.id_topic = p.topic_id
+                    WHERE u.id_user = :id";
 
             return $this->getMultipleResults(
                 DAO::select($sql, ['id' => $id], true), 
