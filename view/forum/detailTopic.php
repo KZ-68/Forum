88620d6b245c7f65e8ce3e2f632e5f765425e323
Kevin ZITNIK <zitnik.kevin@gmail.com>
2023-07-04 22:25:25 +0200

    style: add div tag, css class and id for post area

--- view/forum/detailTopic.php
+++ view/forum/detailTopic.php
@@ -16,6 +16,7 @@ $posts = $result["data"]['posts'];
 foreach($posts as $post){
 ?>
     <div class="topic-posts">
+    <h3>Re: <?=$topics->getTitle()?></h3> 
     <p><a href="index.php?ctrl=forum&action=detailUser&id=<?=$post->getUser()->getId()?>"><?=$post->getUser()->getUsername()?></a></p>
     <p><?=$post->getText()?></p>
     <p><?=$post->getCreationdate()?></p>
@@ -24,12 +25,15 @@ foreach($posts as $post){
 }
 ?>
 
-<form action="index.php?ctrl=forum&action=createPost&id=<?=$topics->getId()?>" method="post">
+<div class="postsArea">
+<h4>Add a new post</h4>
+<form id="postsForm" action="index.php?ctrl=forum&action=createPost&id=<?=$topics->getId()?>" method="post">
 <label for="text">Text: </label><br/>
 <textarea id="text" name="text" rows="5" cols="50"></textarea>
 
 <input type="hidden" id="topic_id" name="topic_id" value="<?=$topics->getId()?>">
-<input id="submit" type="submit" name="createPost" value="Add new post">
+<input id="submitPost" type="submit" name="createPost" value="Add new post">
 </form>
+</div>
 
 <button class="home-btn"><a href="index.php?ctrl=home&action=home.php">Return Home</a></button>
