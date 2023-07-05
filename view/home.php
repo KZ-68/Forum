13ae646ed85a47aba5945aff862e34a9d556fa0c
Kevin ZITNIK <zitnik.kevin@gmail.com>
2023-07-05 23:15:16 +0200

    fix: change redirection link for loginForm

--- view/home.php
+++ view/home.php
@@ -4,7 +4,7 @@
 <p id="home-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit ut nemo quia voluptas numquam, itaque ipsa soluta ratione eum temporibus aliquid, facere rerum in laborum debitis labore aliquam ullam cumque.</p>
 
 <p id="access">
-    <a href="/security/login.html">Se connecter</a>
+    <a href="index.php?ctrl=security&action=loginForm">Se connecter</a>
     <span>&nbsp;&nbsp;</span>
     <a href="index.php?ctrl=security&action=registerForm">S'inscrire</a>
 </p>
