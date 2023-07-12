78d2741fc8312ef7a64ea3606b86082a7adb4590
Kevin ZITNIK <zitnik.kevin@gmail.com>
2023-07-12 19:40:24 +0200

    fix: remove irrelevant structure controll + change variable declaration to "let"

--- public/js/formsActions.js
+++ public/js/formsActions.js
@@ -1,30 +1,23 @@
 function confirmEmail() {
-    var email = document.getElementById("email").value
-    var confemail = document.getElementById("confEmail").value
+    let email = document.getElementById("email").value
+    let confemail = document.getElementById("confEmail").value
     if(email != confemail) {
         alert('Email Not Matching!');
     }
 }
 
 function togglePassword() {
-    var x = document.getElementById("pass");
-    var y = document.getElementById("confPass");
+    let x = document.getElementById("pass");
     if (x.type === "password") {
       x.type = "text";
     } else {
       x.type = "password";
-    }
-    if (y.type === "password") {
-      y.type = "text";
-    } else {
-      y.type = "password";
-    }
-    
+    } 
   }
 
 function confirmPassword() {
-    var pass = document.getElementById("pass").value
-    var confpass = document.getElementById("confPass").value
+    let pass = document.getElementById("pass").value
+    let confpass = document.getElementById("confPass").value
     if(pass != confpass) {
         alert('Password Not Matching!');
     }
