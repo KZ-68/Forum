588e2c516d9940a32fe65f0c998e3c17155d3898
Kevin ZITNIK <zitnik.kevin@gmail.com>
2023-07-12 18:27:00 +0200

    feat:add structures controls for register + login

--- controller/SecurityController.php
+++ controller/SecurityController.php
@@ -16,43 +16,55 @@
         public function registerForm() {
 
             return [
-                "view" => VIEW_DIR."security/register.html"
+                "view" => VIEW_DIR."security/register.php"
             ];
 
         }
 
         public function register() {
             $userManager = new UserManager();
+            $session = new Session();
 
+            // Je filtre les saisies du formulaire
+            $username = filter_input(INPUT_POST, 'username', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+            $email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_FULL_SPECIAL_CHARS, FILTER_VALIDATE_EMAIL);
             $password = filter_input(INPUT_POST, 'password', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
             $confirmPass = filter_input(INPUT_POST, 'confirmPassword', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
-            $username = filter_input(INPUT_POST, 'username', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
-            $email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
 
+            // Je cherche dans la base de données les utilisateurs existants par leur email et leur pseudonyme
             $emailExist = $userManager->findUserByEmail($email); 
             $usernameExist = $userManager->findUserByUsername($username);
 
-            if ($emailExist) {
-                exit("Email adress already exist");
-            } else if ($usernameExist) {
-                exit("Username already exist");
-            }
-            
-            if ($password == $confirmPass) {
-                $passwordKey = password_hash($password, PASSWORD_DEFAULT);
-                return [
-                    "data" => [
-                        $userManager->add(['username' => $username, 'password' => $passwordKey, 'email' => $email])
-                    ]
-                ];
-            }
+            // Si les champs sont valides :
+            if ($username && $email && $password && $confirmPass) {
+                // Je vérifie que l'email et le pseudonyme n'est pas déjà présent en bdd
+                if ($emailExist) {
+                    // Dans le cas contraire, j'affiche un message d'erreur générique.
+                    return [
+                        "view" => VIEW_DIR."security/register.php",
+                        $session->addFlash('error',"Email adress or Username already exist")
+                    ];
+                } else if ($usernameExist) {
+                    return [
+                        "view" => VIEW_DIR."security/register.php",
+                        $session->addFlash('error',"Email adress or Username already exist")
+                    ];
+                    } else {
+                        // Je vérifie que le mot de passe est identique au second et que sa taille fait minimum 8 caractères (à développer vers un regex)
+                        if ($password == $confirmPass && strlen($password) >= 8) {
+                            // Et si tout est bon, j'insère en base de donnée en utilisant password_hash sur le mot de passe.
+                            $userManager->add(['username' => $username, 'password' => password_hash($password, PASSWORD_DEFAULT), 'email' => $email]);
+                            header('Location : security/registerForm.php'); exit;
+                        }
+                    }
+            } 
 
         }
 
         public function loginForm() {
 
             return [
-                "view" => VIEW_DIR."security/login.html"
+                "view" => VIEW_DIR."security/login.php"
             ];
 
         }
@@ -60,29 +72,37 @@
         public function login() {
             
             $userManager = new UserManager();
+            $session = new Session();
             
             $username = filter_input(INPUT_POST, 'username', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
             $password = filter_input(INPUT_POST, 'password', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
             
             $user = $userManager->findUserByUsername($username);
 
-            if ($user) {
-                $userManager->checkPass($username);
-                password_verify($password, $user->getPassword()); 
-                // On récupère le mot de passe
-            } else {
-                exit("Username not found !");
+            if ($username && $password) {
+                if ($user) {
+                    $hash = $user->getPassword(); 
+                    if (password_verify($password, $hash)) {
+                        $session = new Session();
+                        return [
+                            $session->setUser($user),
+                            "view" => VIEW_DIR."forum/categoriesList.php"
+                        ];
+                    } else {
+                        $session->addFlash('error',"Incorrect or inexistant password");
+                    }
+                    // On récupère le mot de passe
+                } else {
+                    $session = new Session();
+                        return [
+                            "view" => VIEW_DIR."security/login.php",
+                            $session->addFlash('error',"Username or password not found")
+                        ];
+                }
             }
+            
 
-            if ($userManager->checkPass($username) && password_verify($password, $user->getPassword())) {
-                $session = new Session;
-                return [
-                    $session->setUser($user),
-                    "view" => VIEW_DIR."forum/categoriesList.php"
-                ];
-            } else {
-                exit("Incorrect Password !");
-            }
+            
             
         }
     }
