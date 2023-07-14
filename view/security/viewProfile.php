0aa51f52d60c3843cd905b6f942a6e4a4db799fe
Kevin ZITNIK <zitnik.kevin@gmail.com>
2023-07-14 23:20:38 +0200

    feat: add viewProfile file + add display role by role in Session (unfinished)

---
+++ view/security/viewProfile.php
@@ -0,0 +1,31 @@
+<?php
+
+$user = $result["data"]['user'];
+
+?>
+
+<img src="public/img/<?=$user->getAvatar()?>">
+<form action="index.php?ctrl=forum&action=changeAvatar&id=<?=$user->getId()?>" method="post">
+<label>Change Avatar: </label><br/>
+<input type="file" id="avatar" name="avatar">
+<input id="submit" type="submit" name="changeAvatar" value="Confirm">
+</form>
+
+
+<p>User Name : <?=$user->getUsername()?></p>
+<p>Registration Date : <?=$user->getRegistrationdate()?></p>
+<p>Email Adress : <?=$user->getEmail()?></p>
+<?php
+if (App\Session::getUser()->hasRole("ROLE_USER")) {
+?>
+<p>Role : User</p>
+<?php
+} else if (App\Session::isAdmin()) {
+?>
+<p>Role : Administrator</p> 
+<?php
+}
+?>
+
+
+<button class="home-btn"><a href="index.php?ctrl=home&action=home.php">Return Home</a></button>
