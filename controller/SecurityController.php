33b6039502b94a4cd9cbf629672780b227a1254a
Kevin ZITNIK <zitnik.kevin@gmail.com>
2023-07-15 19:33:41 +0200

    feat: add success message for submit + rename somes input post

--- controller/SecurityController.php
+++ controller/SecurityController.php
@@ -175,14 +175,13 @@
             $userManager = new UserManager();
             $session = new Session();
             
-            $oldPassword = filter_input(INPUT_POST, 'oldPassword', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
-            $password = filter_input(INPUT_POST, 'password', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+            $oldPassword = filter_input(INPUT_POST, 'password', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+            $password = filter_input(INPUT_POST, 'newPassword', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
             $confirmPass = filter_input(INPUT_POST, 'confirmPassword', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
 
             $user = $userManager->findOneById($id);
 
             if ($_POST['update']) {
-                die(var_dump($user));
                 // Vérifie les filtres
                 if ($oldPassword && $password && $confirmPass) {
                     // Vérifie si l'utilisateur existe
@@ -198,7 +197,8 @@
                                 if ($password === $confirmPass && preg_match($password_regex, $password)) {
                                     $userManager->updatePassword(password_hash($password, PASSWORD_DEFAULT), $id);
                                     return [
-                                        header("Location: index.php?ctrl=security&action=updateUserAccountForm&id=".$user->getId()."")
+                                        header("Location: index.php?ctrl=security&action=updateUserAccountForm&id=".$user->getId().""),
+                                        $session->addFlash('success',"Password successfully changed !")
                                     ];
                                 }
                             }
